f793274a1e0a5e8bad07d9434d45de4a020e3d0f
YangMr <759168442@qq.com>
2021-04-04 20:58:41 +0800

    数据库和迁移配置

--- egg-live/config/config.default.js
+++ egg-live/config/config.default.js
@@ -43,6 +43,31 @@ module.exports = appInfo => {
 		allowMethods: 'GET, PUT, POST, DELETE, PATCH'
 	};
 
+	//数据库配置
+	config.sequelize = {
+		dialect: 'mysql',
+		host: '127.0.0.1',
+		username: "root",
+		password: 'root',
+		port: 3306,
+		database: 'egg-live',
+		// 中国时区
+		timezone: '+08:00',
+		define: {
+			// 取消数据表名复数
+			freezeTableName: true,
+			// 自动写入时间戳 created_at updated_at
+			timestamps: true,
+			// 字段生成软删除时间戳 deleted_at
+			// paranoid: true,
+			createdAt: 'created_time',
+			updatedAt: 'updated_time',
+			// deletedAt: 'deleted_time',
+			// 所有驼峰命名格式化
+			underscored: true
+		}
+	};
+
 
 	return {
 		...config,
--- egg-live/config/plugin.js
+++ egg-live/config/plugin.js
@@ -2,13 +2,18 @@
 
 /** @type Egg.EggPlugin */
 module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
-  
-  cors:{
-    enable: true,
-    package: 'egg-cors',
-  }
+	// had enabled by egg
+	// static: {
+	//   enable: true,
+	// }
+
+	cors: {
+		enable: true,
+		package: 'egg-cors',
+	},
+
+	sequelize : {
+		enable: true,
+		package: 'egg-sequelize',
+	}
 };
